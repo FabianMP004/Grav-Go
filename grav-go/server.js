8c062f28cd350538bc3d134b6e9e781fc4fcf09e
Fabian.MP.04 <fabianmiranda@ufm.edu>
2025-11-19 17:37:03 -0600

    undate server.js
    update

--- grav-go/server.js
+++ grav-go/server.js
@@ -11,19 +11,10 @@ app.use(express.json());
 
 // CORS middleware - allow requests from React dev server
 app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  // Allow requests from common React dev server ports
-  if (origin && (origin.includes('localhost:3000') || origin.includes('localhost:5001'))) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  
-  // Handle preflight requests
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
-  
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
   next();
 });
 
