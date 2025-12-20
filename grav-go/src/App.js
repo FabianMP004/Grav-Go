c5270270a6bf1b956e2cb3c35698ebc1c327a5fe
Fabian.MP.04 <fabianmiranda@ufm.edu>
2025-12-20 10:23:19 -0600

    camiob de url
    url correcta

--- grav-go/src/App.js
+++ grav-go/src/App.js
@@ -58,7 +58,7 @@ function App(){
       throw new Error('No user logged in');
     }
     try{
-      const res = await fetch('/api/auth/topup', {
+      const res = await fetch('https://grav-go.onrender.com/api/auth/topup', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email: currentUser.email, amount })
